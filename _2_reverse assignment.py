9043f185f613b950895e6aba1a68f5e9b1282303
Sanjay Sangma <46703516+Sanjaysangma@users.noreply.github.com>
2022-09-16 18:59:55 +0530

    Update _2_reverse assignment.py

--- _2_reverse assignment.py
+++ _2_reverse assignment.py
@@ -1,16 +1,11 @@
-# #
-# Word = input("Input a word to revers:  " )
+#Write a Python program that accepts a word from the user and reverse it.
 
-# for char in range(len(Word) -1,-1,-1):
-#     print(Word[char], end="")
-# print("\n")
+Word = input("Input a word to reverse:  " )
+
+for char in range(len(Word) -1,-1,-1):
+    print(Word[char], end="")
+print("\n")
 
 
 
-str1 = input("enter the sentence: ")
-words = str1.split()
-reversed_str1= []
-for i in words:
-    reversed_str1.append(i[::-1])
-print(reversed_str1)
 
